7a14a62d4494c2e5794c7df0a4bb46dee489b9a5
Seokhoon Lee <seokhoon.lee.9393@gmail.com>
2015-09-25 16:18:01 +0800

    Add StorageManager.java

---
+++ StorageManager.java
@@ -0,0 +1,30 @@
+/**
+ * StorageManager is a class that read/write/delete appropriate task information to the Storage.
+ */
+public class StorageManager {
+	private String fileName = "TaskStorage";
+	private String directory = "./";
+
+	public StorageManager() {
+	}
+
+	public boolean openStorage() {
+
+	}
+
+	public boolean closeStorage() {
+
+	}
+
+	public boolean writeTask(Task task) {
+
+	}
+
+	public Task readTask(String name) {
+
+	}
+
+	public Task[] readAllTask() {
+
+	}
+}
