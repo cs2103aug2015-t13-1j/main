785fd35e6d773dba5ca85c4e5237f71b7e21ebe2
Seokhoon Lee <seokhoon.lee.9393@gmail.com>
2015-09-25 17:01:58 +0800

    Commit to checkout

--- StorageManager.java
+++ StorageManager.java
@@ -1,23 +1,55 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 /**
  * StorageManager is a class that read/write/delete appropriate task information to the Storage.
  */
 public class StorageManager {
-	private String fileName = "TaskStorage";
-	private String directory = "./";
+	private final String FILE_NAME = "TaskStorage";
+	private final String FILE_TYPE = ".json"
+	private final String DIRECTORY = "./";
+	private FileWriter fileWriter;
+	private BufferedWriter bufferedWriter;
 
 	public StorageManager() {
 	}
 
 	public boolean openStorage() {
+		try {
+			File file = new File(DIRECTORY + FILE_NAME + FILE_TYPE);
 
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+
+			fileWriter = new FileWriter(file.getAbsoluteFile());
+			bufferedWriter = new BufferedWriter(fileWriter);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public boolean closeStorage() {
-
+		try {
+			bufferedWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public boolean writeTask(Task task) {
 
+
+		try {
+			bufferedWriter.write(task);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public Task[] readTask(String name) {
