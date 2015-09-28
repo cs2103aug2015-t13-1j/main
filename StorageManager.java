98be2bec1c10b69185fe57925610e953da481dde
Seokhoon Lee <seokhoon.lee.9393@gmail.com>
2015-09-28 16:39:50 +0800

    Commit modified changes

--- StorageManager.java
+++ StorageManager.java
@@ -1,19 +1,26 @@
 import java.io.File;
-import java.io.FileWriter;
-import java.io.BufferedWriter;
 import java.io.IOException;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 /**
  * StorageManager is a class that read/write/delete appropriate task information to the Storage.
  */
 public class StorageManager {
+	private final String DIRECTORY = "./";
 	private final String FILE_NAME = "TaskStorage";
 	private final String FILE_TYPE = ".json"
-	private final String DIRECTORY = "./";
+	private FileReader fileReader;
 	private FileWriter fileWriter;
+	private BufferedReader bufferedReader;
 	private BufferedWriter bufferedWriter;
 
 	public StorageManager() {
@@ -27,7 +34,9 @@ public class StorageManager {
 				file.createNewFile();
 			}
 
+			fileReader = new FileReader(file.getAbsoluteFile());
 			fileWriter = new FileWriter(file.getAbsoluteFile());
+			bufferedReader = new BufferedWriter(fileReader);
 			bufferedWriter = new BufferedWriter(fileWriter);
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -36,27 +45,76 @@ public class StorageManager {
 
 	public boolean closeStorage() {
 		try {
+			bufferedReader.close();
 			bufferedWriter.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public boolean writeTask(Task task) {
-
-
-		try {
-			bufferedWriter.write(task);
+			fileReader.close();
+			fileWriter.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
 	public Task[] readTask(String name) {
+		JSONParser jsonParser = new JSONParser();
 
+		try {
+			Object obj = jsonParser.parse(bufferedReader);
+
+			JSONArray jsonArray = (JSONArray) obj; 
+			Task[] taskList = new Task[jsonArray.length()];
+
+			if (jsonArray != null) { 
+				for (int i = 0; i < jsonArray.length(); i++) {
+					if (jsonArray.get(i).name.equals(name)) {
+						taskList[i] = jsonArray.get(i); // SKSK: Maybe need to convert Object (jsonArray.get(i)) to Task
+					}
+				}
+			}
+
+			return taskList;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public Task[] readAllTask() {
+		JSONParser jsonParser = new JSONParser();
 
+		try {
+			Object obj = jsonParser.parse(bufferedReader);
+
+			JSONArray jsonArray = (JSONArray) obj; 
+			Task[] taskList = new Task[jsonArray.length()];
+
+			if (jsonArray != null) { 
+				for (int i = 0; i < jsonArray.length(); i++) {
+					taskList[i] = jsonArray.get(i); // SKSK: Maybe need to convert Object (jsonArray.get(i)) to Task
+				}
+			}
+
+			return taskList;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void writeTask(Task task) {
+		JSONParser jsonParser = new JSONParser();
+
+		try {
+			Object obj = jsonParser.parse(bufferedReader);
+
+			JSONArray jsonArray = (JSONArray) obj; 
+			jsonArray.add(task);
+
+			bufferedWriter.write(jsonArray.toJSONString());
+
+			return;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
