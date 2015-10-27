131eb416d6cf3b24882ab028c81a88b85e5e6ea0
Dickson <dickson.tan.2013@gmail.com>
2015-10-27 14:37:19 +0800

    Added a DeltaTask class to store info about the changes that users want to make in an Update command. Used mainly by CommandParser and Update to communicate. Added parsing of the update command, so that entered info is stored in DeltaTask.

---
+++ DeltaTask.java
@@ -0,0 +1,54 @@
+import java.time.LocalDateTime;
+
+/*
+ *This class stores info about what changes users want to make to an existing task
+ *Used by CommandParser to communicate with the Update command class.  
+ *@author Dickson
+ */
+
+public class DeltaTask {
+private String name;
+private LocalDateTime start, end;
+
+// null cannot be used to indicate the absence of changes, because we must differentiate between 
+// the user making no change, or wanting to delete information in that field
+public enum FIELD_ACTION {
+	NONE, UPDATE, REMOVE
+}
+
+private FIELD_ACTION nameAction, startAction, endAction;
+
+public DeltaTask(FIELD_ACTION nAction, String newName, FIELD_ACTION sAction, LocalDateTime newStart, FIELD_ACTION eAction, LocalDateTime newEnd) {
+	nameAction = nAction;
+	startAction = sAction;
+	endAction = eAction;
+	name = newName;
+	start = newStart;
+	end = newEnd;
+}
+
+public FIELD_ACTION getNameAction() {
+	return nameAction;
+}
+
+public FIELD_ACTION getStartAction() {
+	return startAction;
+}
+
+public FIELD_ACTION getEndAction() {
+	return endAction;
+}
+
+public String getNewName() {
+	return name;
+}
+
+public LocalDateTime getNewStart() {
+	return start;
+}
+
+public LocalDateTime getNewend() {
+	return end;
+}
+
+}
