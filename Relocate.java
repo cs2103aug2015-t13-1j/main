db25ce72f40ef46d103fb856728466e01c295adb
Seokhoon Lee <seokhoon.lee.9393@gmail.com>
2015-11-08 16:42:26 +0800

    Add lines after new logging

--- Relocate.java
+++ Relocate.java
@@ -1,6 +1,7 @@
 //@@author A0100081E
 public class Relocate extends Command {
 	private final String SUCCESS_RELOCATE = "Storage was relocated to \"%s\"";
+	private final String ERROR_FILE_NOT_FOUND = "File directory specified does not exist";
 	private String fileLocation = null;
 	
 	public Relocate(String fileLocation) {
@@ -11,7 +12,7 @@ public class Relocate extends Command {
 	@Override
 	public void execute() throws Exception {
 		if (!storageManager.changeStorageLocation(fileLocation)) {
-			throw new Exception("File directory specified does not exist");
+			throw new Exception(ERROR_FILE_NOT_FOUND);
 		}
 	}
 
