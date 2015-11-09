95b045da476499c59bdfc008eff1c8e35d906f70
Dickson <dickson.tan.2013@gmail.com>
2015-11-09 20:56:36 +0800

    Add folder path validation inside the move class, haven't removed the path validation from CommandParser yet. Implemented undo for move.

--- Move.java
+++ Move.java
@@ -1,29 +1,44 @@
+import java.io.File;
+import java.util.logging.Level;
+
 //@@author A0100081E
-public class Move extends Command {
-	private final String SUCCESS_MOVE = "Storage was moved to \"%s\"";
-	private final String ERROR_FILE_NOT_FOUND = "File directory specified does not exist";
-	private String fileLocation = null;
+public class Move extends Command implements Undoable {
+	private static final String SUCCESS_MOVE = "Storage was moved to \"%s\"";
+	private static final String SUCCESS_UNDO = "Storage was moved back to the original location \"%s\"";
+	private static final String ERROR_INVALID_FOLDER_PATH = "%s is not a valid path to an existing folder.";
+	private String newLocation = null;
+	private String oldLocation = null;
 	
 	public Move(String fileLocation) {
 		assert(fileLocation != null);
-		this.fileLocation = fileLocation;
+		newLocation = fileLocation;
 	}
 	
 	@Override
 	public void execute() throws Exception {
-		if (!storageManager.changeStorageLocation(fileLocation)) {
-			throw new Exception(ERROR_FILE_NOT_FOUND);
+		oldLocation = storageManager.getStorageDirectory();
+		System.out.println("old location = " + oldLocation);
+		File newPath = new File(newLocation);
+		if (newPath.isDirectory() == false) {
+	  		log.log(Level.INFO, "aborting because the folder path is invalid\n");
+	  		throw new Exception(String.format(ERROR_INVALID_FOLDER_PATH, newLocation));
+	  	}
+		storageManager.changeStorageLocation(newLocation);
 		}
-	}
+	
 
 	@Override
 	public String getSuccessMessage() {
-		return String.format(SUCCESS_MOVE, fileLocation);
+		return String.format(SUCCESS_MOVE, newLocation);
 	}
 	
 	//@@author A0126270N
-	private String getFileLocation() {
-		return fileLocation;
+	public String getNewLocation() {
+		return newLocation;
+	}
+	
+	public String getOldLocation() {
+		return oldLocation;
 	}
 	
 	public boolean equals(Object obj) {
@@ -36,6 +51,20 @@ public class Move extends Command {
 		}
 		
 		Move other = (Move)obj;
-		return fileLocation.equals(other.getFileLocation()); 		
+		boolean isOldLocationEqual = (oldLocation == null && other.getOldLocation() == null) || (oldLocation != null && oldLocation.equals(other.getOldLocation()));
+		return isOldLocationEqual && newLocation.equals(other.getNewLocation()); 		
 	}
+	
+	public void undo() throws Exception {
+		File oldPath = new File(oldLocation);
+		if (oldPath.isDirectory() == false) {
+	  		log.log(Level.INFO, "aborting because the old folder path is invalid\n");
+	  		throw new Exception("Undo failed. " + String.format(ERROR_INVALID_FOLDER_PATH, oldLocation));
+	  	}
+		storageManager.changeStorageLocation(oldLocation);
+	}
+	
+	public String getUndoMessage() {
+		return String.format(SUCCESS_UNDO, oldLocation);
+}
 }
